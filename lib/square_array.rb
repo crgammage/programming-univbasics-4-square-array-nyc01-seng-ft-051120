f3012ae8c13fc7413c62e88db055cc6d8ef6cf22
masterful-array-3850 <crgammage@gmail.com>
2020-04-22 16:13:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.length.times do |index|
-  square = array[index] * array[index]
+  square = array[index]**2
   new_array.push(square)
   return new_array
 end
