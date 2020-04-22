efcef5bf3dc25240fb9771a6d18ebbd44b03fca2
masterful-array-3850 <crgammage@gmail.com>
2020-04-22 16:10:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   counter = 0
+  new_array = []
   while counter < array.length
-  array[counter] * array[counter]
-  return array
+  square = array[counter] ** 2
+  new_array.push(square)
+  return new_array
 end
 end
